97763e96bac7a4f244619e5a9fc87a2e78bec3eb
kaynzei <kanayo4christ@gmail.com>
2023-10-27 16:49:21 -0700

    weatherdetector plus vercel.json file for deployment to vercel

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y1hfu-zb8crzyj^q&^pl@r6u#%an*cl1gz4$rvp85^$7e5bzhr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
